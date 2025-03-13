99486cfe7da39473421babf90a9ff3055bc461cd
RosyPetrella <petrellamr@gmail.com>
2025-03-13 16:36:36 +0100

    edit bonus

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3005;
-const postsRouter = require("./routers/posts.js");
+const postsRouter = require("./routers/posts");
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 app.use("/api/v1/posts", postsRouter);
+// app.use(express.json());
 
 // show
 app.get("/", (req, res) => {
