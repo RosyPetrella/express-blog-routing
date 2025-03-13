c5644bb500e0f174fc35bb4e6b35cf409a71f178
RosyPetrella <petrellamr@gmail.com>
2025-03-13 15:24:16 +0100

    add route Show

---
+++ routers/posts.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+let posts = require("../immagini_e_posts/posts.js");
+
+// index
+router.get("/", (req, res) => {
+  res.send("Lista dei post");
+});
+
+// Show
+router.get("/:id", (req, res) => {
+  res.send(`Mostra post con id ${req.params.id}`);
+});
+
+// create
+router.post("/", (req, res) => {
+  res.send("Creazione nuovo post");
+});
+
+// update
+router.put("/:id", (req, res) => {
+  res.send(`Update un post con un nuovo id  ${req.params.id}`);
+});
+
+// Modify
+router.patch("/:id", (req, res) => {
+  res.send(`Modify un post con un nuovo id ${req.params.id}`);
+});
+
+// Delete
+router.delete("/:id", (req, res) => {
+  res.send(`Cancella un post con id ${req.params.id}`);
+});
+
+module.exports = router;
